bc6faa8a2ccf8e14c14f4d6e60d00d3ca4ab4f09
Mahad Saleem <ms.asd871@gmail.com>
2024-03-31 00:18:29 +0500

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,114 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalPlugin =
+    FlutterLocalNotificationsPlugin();
 
+FlutterBackgroundService? service;
+bool? isServiceRunning;
 
-final FlutterLocalNotificationsPlugin flutterLocalPlugin =FlutterLocalNotificationsPlugin();
-const AndroidNotificationChannel notificationChannel=AndroidNotificationChannel(
-    "coding is life foreground",
+const AndroidNotificationChannel notificationChannel = AndroidNotificationChannel(
+    "coding is life", // Changed to match the ID used in the service configuration
     "coding is life foreground service",
     description: "This is channel des....",
-  importance: Importance.high
-);
+    importance: Importance.high);
+void printStatus(String text) {
+  print('====================');
+  print(text);
+  print('====================');
+}
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initservice();
   runApp(const MyApp());
 }
 
-Future<void> initservice()async{
-  var service=FlutterBackgroundService();
-  //set for ios
-  if(Platform.isIOS){
-    await flutterLocalPlugin.initialize(const InitializationSettings(
-      iOS: DarwinInitializationSettings()
-    ));
+Future<void> initservice() async {
+  service = FlutterBackgroundService();
+
+  // Set for iOS
+  if (Platform.isIOS) {
+    await flutterLocalPlugin.initialize(
+        const InitializationSettings(iOS: DarwinInitializationSettings()));
   }
 
-  await flutterLocalPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(notificationChannel);
+  await flutterLocalPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(notificationChannel);
 
-  //service init and start
-  await service.configure(
-      iosConfiguration: IosConfiguration(
-        onBackground: iosBackground,
-        onForeground: onStart
-      ),
+  // Service init and start
+  await service!.configure(
+      iosConfiguration:
+          IosConfiguration(onBackground: iosBackground, onForeground: onStart),
       androidConfiguration: AndroidConfiguration(
-        onStart: onStart,
-        autoStart: true,
-        isForegroundMode: true,
-        notificationChannelId: "coding is life",//comment this line if show white screen and app crash
-        initialNotificationTitle: "Coding is life",
-        initialNotificationContent: "Awsome Content",
-        foregroundServiceNotificationId: 90
-      )
-  );
-  service.startService();
+          onStart: onStart,
+          autoStart: true,
+          isForegroundMode: true,
+          notificationChannelId:
+              "coding is life", // Now matches the ID of the notification channel
+          initialNotificationTitle: "Coding is life",
+          initialNotificationContent: "Awesome Content",
+          foregroundServiceNotificationId: 90));
 
-  //for ios enable background fetch from add capability inside background mode
+  service!.startService();
 
+  int counter = 0;
+  Timer.periodic(Duration(seconds: 1), (timer) async {
+    counter++;
+    print('Counter: $counter');
+    if (counter >= 5) {
+      service!.isRunning().then((value) {
+        value ? service!.invoke("stopService") : print('Service not running');
+      });
+
+      timer.cancel();
+      // Perform any actions after 30 seconds here
+    }
+  });
+
+  // For iOS enable background fetch from add capability inside background mode
 }
 
-//onstart method
+// onStart method
 @pragma("vm:entry-point")
-void onStart(ServiceInstance service){
+void onStart(ServiceInstance service) {
   DartPluginRegistrant.ensureInitialized();
-
   service.on("setAsForeground").listen((event) {
     print("foreground ===============");
   });
-
   service.on("setAsBackground").listen((event) {
     print("background ===============");
   });
-
   service.on("stopService").listen((event) {
     service.stopSelf();
+    isServiceRunning = false;
+    printStatus(isServiceRunning.toString());
   });
-
-  //display notification as service
-  Timer.periodic(Duration(seconds: 2), (timer) {
-    flutterLocalPlugin.show(
-        90,
-        "Cool Service",
-        "Awsome ${DateTime.now()}",
-        NotificationDetails(android:AndroidNotificationDetails("coding is life","coding is life service",ongoing: true,icon: "app_icon")));
-  });
-  print("Background service ${DateTime.now()}") ;
-
+  // Display notification as service
 }
 
 //iosbackground
 @pragma("vm:entry-point")
-Future<bool> iosBackground(ServiceInstance service)async{
+Future<bool> iosBackground(ServiceInstance service) async {
   WidgetsFlutterBinding.ensureInitialized();
   DartPluginRegistrant.ensureInitialized();
 
   return true;
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -105,43 +116,42 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            //layout
-            SizedBox(height: 200,),
-            ElevatedButton(onPressed: (){
-              FlutterBackgroundService().invoke("stopService");
-            }, child: Text("stop service")),
-            ElevatedButton(onPressed: (){
-              FlutterBackgroundService().startService();
-            }, child: Text("start service")),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Background Service'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              isServiceRunning != null
+                  ? isServiceRunning!
+                      ? Text(
+                          'Service is running',
+                          style: TextStyle(color: Colors.green),
+                        )
+                      : Text(
+                          'Service is stopped',
+                          style: TextStyle(color: Colors.red),
+                        )
+                  : Text('Service is not running'),
+              ElevatedButton(
+                onPressed: () async {
+                  await initservice();
+                  setState(() {
+                    isServiceRunning = true;
+                  });
+                },
+                child: Text('Start Background Service'),
+              ),
+              ElevatedButton(
+                onPressed: () async {},
+                child: Text('Stop Background Service'),
+              ),
+            ],
+          ),
         ),
       ),
-
     );
   }
 }
